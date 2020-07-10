451d6cdcaf14249b0f072c71761938bfe3c8f1c1
Vestroie <heroprotunlong@gmail.com>
2020-07-10 18:31:04 -0400

    Added gates

---
+++ GMTK Game Jam/Assets/Scripts/Gate.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    // Variables
+    [SerializeField]
+    public bool isVertical = false;
+
+    public float speed;
+
+    float timer;
+    bool isOpen;
+    bool isMoving;
+    Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0f;
+        isOpen = false;
+        isMoving = false;
+
+        moveDirection = isVertical ? transform.up : transform.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isMoving)
+        {
+            MoveGate();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if(!isMoving)
+        {
+            isOpen = !isOpen;
+            isMoving = true;
+        }
+    }
+
+    private void MoveGate()
+    {
+        if(timer >= 1f)
+        {
+            timer = 0f;
+            isMoving = false;
+            return;
+        }
+
+        timer += Time.deltaTime;
+
+        transform.position = (Vector2)transform.position + moveDirection * speed * Time.deltaTime;
+    }
+}
