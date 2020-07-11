9cd5a24f39fdcc5794504d11aae2b442cfe49615
Vestroie <heroprotunlong@gmail.com>
2020-07-11 17:33:29 -0400

    Start menu

---
+++ GMTK Game Jam/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public int gameScene;
+    public GameObject optionMenu;
+    public GameObject mainMenu;
+
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync(gameScene);
+    }
+
+    public void OptionToggle()
+    {
+        if(optionMenu.activeInHierarchy)
+        {
+            optionMenu.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+        else
+        {
+            optionMenu.SetActive(true);
+            mainMenu.SetActive(false);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
