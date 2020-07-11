9f77057fbc52f46291cbc418ec1d77b8f59c81d2
thien-pham28 <59208257+thien-pham28@users.noreply.github.com>
2020-07-10 22:13:46 -0500

    Implemented Grid Highlight

---
+++ GMTK Game Jam/Assets/Scripts/GridHighlight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridHighlight : MonoBehaviour
+{
+    public Tile highlightTile;
+    public Tilemap highlightMap;
+
+    private Vector3Int previousCell;
+
+    // do late so that the player has a chance to move in update if necessary
+    private void LateUpdate()
+    {
+        // get current grid location
+        Vector3Int currentCell = highlightMap.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+        // if the position has changed
+        if (currentCell != previousCell)
+        {
+            // set the new tile
+            highlightMap.SetTile(currentCell, highlightTile);
+
+            // erase previous
+            highlightMap.SetTile(previousCell, null);
+
+            // save the new position for next frame
+            previousCell = currentCell;
+        }
+    }
+}
